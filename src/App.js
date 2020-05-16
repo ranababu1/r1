43380666ba8f70c796788ea35800b37fcf6568bb
ranababu1 <imurokks@gmail.com>
2020-05-16 19:15:10 +0530

    text changes

--- src/App.js
+++ src/App.js
@@ -6,37 +6,34 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div className="jumbotron dark">
-          <h1 className="jumboH1">Age of Mythology</h1>
+        <div className="jumbotron">
+          <h1 className=" Display-1">Mythology</h1>
         </div>
 
         <div className="main">
           <div>
-            <div className="col-md-12 colorfulBlock">
-              <h2 className="jumboH2">Know about the characters of mythic age</h2>
-            </div>
-
-
             <div className="col-md-12 colorfulBlock3">
               <div className="container">
                 <div className=" col-md-4">
-                  <div className=" threebox"><p className="threeboxtext">Greek</p></div>
+                  <div className=" threebox">
+                    <p className="threeboxtext">Greek</p>
+                  </div>
                 </div>
                 <div className=" col-md-4">
-                  <div className=" threebox"><p className="threeboxtext">Egyptian</p></div>
+                  <div className=" threebox">
+                    <p className="threeboxtext">Egyptian</p>
+                  </div>
                 </div>
                 <div className=" col-md-4">
-                  <div className=" threebox"><p className="threeboxtext"></p>Norse</div>
+                  <div className=" threebox">
+                    <p className="threeboxtext">Norse</p>
+                  </div>
                 </div>
               </div>
             </div>
-
-
           </div>
 
         </div>
-
-
 
         <div className="footer">
           <p className="footerText">made with <span className="heartbeating"> &#10084; </span>using react</p>
